22cacba8fbc843cabeb236a2b3457599098e2ad4
vlad-86 <cooldude@list.ru>
2017-02-02 22:41:20 +0200

    adopted script to actual

--- searchproducts.py
+++ searchproducts.py
@@ -1,34 +1,41 @@
+import selenium
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 
 
-profile = webdriver.FirefoxProfile('c:/Users/user\AppData\Local\Mozilla\Firefox\Profiles\dnycs6g4.py_test')
-driver = webdriver.Firefox(profile)
-driver.get("http://demo.magentocommerce.com/")
+# create a new Firefox session
+driver = webdriver.Firefox()
 driver.implicitly_wait(10)
-# get the search textbox
-search_icon = driver.find_element_by_xpath(".//*[@id='nav-main']/div/div[2]/div[1]/a[1]/i")
-search_icon.click()
+driver.maximize_window()
 
-search_field =driver.find_element_by_xpath(".//*[@id='edit-keys']")
-search_field.clear()
+
+# navigate to the application home page
+driver.get("http://demo.magentocommerce.com/")
+
+
+# get the search textbox
+#search_field = driver.find_elements_by_css(".fa.fa-search")
+search_field = driver.find_element_by_xpath(".//*[@id='nav-main']/div/div[2]/div[1]/a[2]/i")
+
+search_field.click()
 
 # enter search keyword and submit
-search_field.send_keys("phones")
-search_field.submit()
+search_field_extended = driver.find_element_by_id('edit-keys')
+search_field_extended.clear()
+search_field_extended.send_keys("phones")
+search_field_extended.submit()
 
 # get all the anchor elements which have product names displayed
 # currently on result page using find_elements_by_xpath method
-products = driver.find_elements_by_xpath(".//*[@id='gl-container']/div[2]/div/div[2]/ul/li[1]/div[1]/a")
+products = driver.find_elements_by_class_name("result-title")
 
 # get the number of anchor elements found
-print("Found " + str(len(products)) + " products:")
+print ("Found " + str(len(products)) + " products:")
 
 # iterate through each anchor element and print the text that is
 # name of the product
 for product in products:
-    print(product.text)
+    print (product.text)
 
 # close the browser window
 driver.quit()
